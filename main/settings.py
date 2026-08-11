825d172f86bf1f9ed2b73f41dd66bf0297550e30
Nana Kwaku <nksarps@gmail.com>
2026-08-11 11:46:01 +0000

    feat(api): register custom exception handler in settings

--- main/settings.py
+++ main/settings.py
@@ -138,6 +138,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'EXCEPTION_HANDLER': 'main.exceptions.custom_exception_handler',
 }
 
 SIMPLE_JWT = {
